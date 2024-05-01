f8b9922835077bb447f850e87547d4b02f04830e
leonchrist <am000hw@gmail.com>
2024-05-01 16:35:21 +0200

    fix availability zone on aws

--- terraform/template/quark-single/aws/network.tf
+++ terraform/template/quark-single/aws/network.tf
@@ -1,7 +1,8 @@
+data "aws_availability_zones" "available" {}
 module "aws_net" {
   source            = "../../../raw_modules/aws-network"
   env               = { tags = merge(local.metadata, { Name = "${local.metadata.Project}_${local.metadata.Role}_Vpc_${local.metadata.Owner}", Source = "Terraform" }) }
-  availability_zone = "${var.cloud_region}a"
+  availability_zone = data.aws_availability_zones.available.names[0]
   providers = {
     aws = aws
   }
