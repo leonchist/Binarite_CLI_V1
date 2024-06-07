3d0b9aa94cd1a175d734920b4cec0a51bbe250bf
leonchrist <am000hw@gmail.com>
2024-06-07 10:35:05 +0200

    Add id outputs to bots terraform configuration

--- terraform/template/eoc-bots/aws/outputs.tf
+++ terraform/template/eoc-bots/aws/outputs.tf
@@ -1,3 +1,15 @@
+
+output "resource_ids" {
+  value = {
+    network : {
+      vpc_id : module.aws_net.vpc_id,
+      subnet_id : module.aws_net.subnet_id,
+      security_group_id : module.aws_net.security_group_id
+    },
+    bots : module.eoc-bots.*.instance_id
+  }
+}
+
 output "public_ips_eu" {
   value = module.eoc-bots.*.vm_public_ips
 }
